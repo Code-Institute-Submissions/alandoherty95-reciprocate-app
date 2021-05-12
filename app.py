915895d7b2f12125bda8919cb787c089ffefd3b1
alandoherty95 <alandoherty95@gmail.com>
2021-05-12 12:03:30 +0000

    Redirect to profile after successfullt editting recipe

--- app.py
+++ app.py
@@ -183,10 +183,23 @@ def add_recipe():
 # edit a recipe
 @app.route("/edit_recipe/<recipe_id>", methods=["GET", "POST"])
 def edit_recipe(recipe_id):
+    if request.method == "POST":
+        submit = {
+            "category_name": request.form.get("category_name"),
+            "recipe_name": request.form.get("recipe_name"),
+            "recipe_ingredients": request.form.get("recipe_ingredients"),
+            "recipe_instructions": request.form.get("recipe_instructions"),
+            "created_by": session["user"],
+            "image_url": request.form.get("image_url")
+        }
+        mongo.db.recipes.update({"_id": ObjectId(recipe_id)}, submit)
+        flash("Recipe was successfully updated")
+
     recipe = mongo.db.recipes.find_one({"_id": ObjectId(recipe_id)})
     categories = mongo.db.categories.find().sort("category_name", 1)
     return render_template(
-        "edit_recipe.html", recipe=recipe, categories=categories)
+        "profile", username=session["user"],
+        recipe=recipe, categories=categories)
 
 
 # delete a recipe
